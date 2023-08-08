eefa05637ab6b1e6c5f8d9c2b0cc689e8b5b80c6
qoalstn <mousson2020@gmail.com>
2023-08-09 01:08:14 +0900

    데이터베이스 연결

---
+++ management/src/main/java/com/fastcampus/management/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.fastcampus.management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fastcampus.management.repository.MemberRepo;
+
+@Service
+public class MemberService {
+    private final MemberRepo memberRepo;
+
+    @Autowired()
+    public MemberService(MemberRepo memberRepo) {
+        this.memberRepo = memberRepo;
+    }
+
+    public int getNewCustNo() {
+        int newMember = memberRepo.getNewCustNo();
+        return newMember;
+    }
+}
---
+++ management/src/main/webapp/WEB-INF/views/regist.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<strong>고객 등록 화면</strong>
+<div>
+	<label>고객번호 : <input type="text"></label>
+	<label>고객이름 : <input type="text"></label>
+	<label>전화번호 : <input type="text"></label>
+	<label>이메일 : <input type="text"></label>
+	<label>고객등급(P,G,S) : <input type="text"></label>
+</div>
+
+</body>
+</html>
